a892424a260a1360eded79b307bd0cf928d731e3
Daniil <danilka.blinov16@gmail.com>
2025-01-19 18:31:57 +0300

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,62 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex h-[100vh]">
+      <div className="flex flex-col gap-6 p-5 bg-white mt-1 m-2 shadow-lg rounded-lg">
+        <div className="bg-slate-500 w-[80px] h-[80px] rounded-[50%] border-2 border-black cursor-pointer"></div>
+        <Link href="/">Главная</Link>
+        <Link href="/notification">Уведомления</Link>
+        <Link href="/messages">Сообщения</Link>
+        <Link href="/friends">Друзья</Link>
+        <Link href="/settings">Настройки</Link>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="flex flex-col bg-white mt-1 m-2 shadow-lg rounded-lg w-[500px]">
+        <div className="flex shadow-sm">
+          <input
+            className="w-full h-[66px] p-2 outline-none rounded-lg"
+            type="text"
+            placeholder="Поиск"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div className="flex gap-3 p-2 border-b-2">
+          <div className="flex flex-col gap-1">
+            <p className="font-bold text-xl">Имя Фамилия</p>
+            <p className="text-gray-500">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque
+              vel dolore enim sint velit consectetur earum quasi odit
+              architecto!
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col w-full bg-white mt-1 m-2 shadow-lg rounded-lg flex-1">
+        <div className="flex p-2 items-center gap-2 shadow-sm">
+          <div className="bg-slate-500 w-[50px] h-[50px] rounded-[50%] border-black cursor-pointer"></div>
+          <p className="font-bold text-xl">Имя Фамилия</p>
+        </div>
+        <div className="flex-1 overflow-x-hidden bg-slate-300">
+          <div className="w-full h-full m-2">
+            <div className="bg-gray-500 rounded-md rounded-bl-none mb-2 ml-3 p-2 w-[350px]">
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque
+              vel dolore enim sint velit consectetur earum quasi odit
+              architecto!</p>
+            </div>
+            <div className="bg-gray-500 w-[20px] h-[20px] rounded-[50%] border-black cursor-pointer"></div>
+          </div>
+        </div>
+        <div className="flex w-full">
+          <textarea
+            className="w-full p-2 outline-none resize-none rounded-lg"
+            placeholder="Написать сообщение"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <button className="bg-slate-500 p-4 text-white rounded-br-lg">
+            Отправить
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
